8cb3f1b5ed99c895795076c6c8e906e968defb10
Shun-Hsing (Earl) Ou <occqoo@gmail.com>
2013-11-28 11:47:07 +0800

    gpu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/ocl/ocl.hpp>
+#include <sys/time.h>
+
 using namespace cv;
+
+//#define OCL
 
 const Scalar RED(0, 0, 255);
 const Scalar GREEN(255, 255, 0);
@@ -11,12 +15,31 @@ void cross(Mat& img, int x, int y, Scalar color)
   line(img, Point(x+5, y-5), Point(x-5, y+5), color);
 }
 
+long getTime()
+{
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
 int main(int argc, char *argv[])
 {
+
+#ifdef OCL
+  vector<ocl::Info> oclInfo;
+  ocl::getDevice(oclInfo);
+  ocl::setDevice(oclInfo[0]);
+#endif
+
   VideoCapture cap(argv[1]);
 
+#ifdef OCL
+  ocl::HOGDescriptor hog;
+  hog.setSVMDetector(ocl::HOGDescriptor::getDefaultPeopleDetector());
+#else
   HOGDescriptor hog;
   hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+#endif
 
   Mat frame;
   Mat map(300, 800, CV_8UC3);
@@ -35,12 +58,26 @@ int main(int argc, char *argv[])
   namedWindow("img");
   namedWindow("map");
   int index = 0;
+  long prevTime = 0;
   while (cap.read(frame))
   {
+    long time = getTime();
+    if (prevTime != 0)
+    {
+      long diffTime = time - prevTime;
+      printf("%f\n", 1000.0f / diffTime);
+    }
+    prevTime = time;
+    Mat grayFrame;
+    cvtColor(frame, grayFrame, CV_BGR2GRAY);
+#ifdef OCL
+    ocl::oclMat calFrame(grayFrame);
+#else
+    Mat calFrame = grayFrame;
+#endif
     Mat mapCanvas = map.clone();
     vector<Rect> found;
-    hog.detectMultiScale(frame, found, 0,
-        Size(8,8), Size(32,32), 1.05, 2);
+    hog.detectMultiScale(calFrame, found, 0);
     for (int i=0, n=found.size(); i<n; ++i)
     {
       rectangle(frame, found[i].tl(), found[i].br(), RED, 3);
